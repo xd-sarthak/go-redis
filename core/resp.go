7f0a19035cd7bc2f33e01dcbab208044d3490ae3
Sarthak Srivastav <sarthak.sri0203@gmail.com>
2026-06-21 14:44:23 +0530

    implemented a simple resp decoder

---
+++ core/resp.go
@@ -0,0 +1,123 @@
+package core
+
+import "errors"
+
+// readLength reads the length of the array or bulk string from the RESP protocol
+/*
+
+suppose redis client sends $5\r\nhello\r\n 
+so here 5 is the length and we also return a delta which is the 
+amount of bytes consumed
+
+*/
+func readLength(data []byte) (int,int){
+	pos,len := 0,0
+
+	for pos = range data {
+		b := data[pos]
+		if !(b >= '0' && b <= '9') {
+			return len, pos+2
+		}
+		len = len*10 + int(b-'0')
+	}
+	return 0,0
+}
+
+// reads a RESP encoded simple string from data and returns
+// the string, the delta, and the error
+func readSimpleString(data []byte) (string,int,error){
+	// check if the first byte is '+' which indicates a simple string
+	pos := 1
+
+	for ; data[pos] != '\r'; pos++ {
+	}
+
+	return string(data[1:pos]), pos+2, nil
+}
+
+// reads a RESP encoded error from data and returns
+// the error string, the delta, and the error
+func readError(data []byte) (string, int, error) {
+	return readSimpleString(data)
+}
+
+// reads a RESP encoded integer from data and returns
+// the intger value, the delta, and the error
+func readInt64(data []byte) (int64, int, error) {
+	// first character :
+	pos := 1
+	var value int64 = 0
+
+	for ; data[pos] != '\r'; pos++ {
+		value = value*10 + int64(data[pos]-'0')
+	}
+
+	return value, pos + 2, nil
+}
+
+
+// reads a RESP encoded string from data and returns
+// the string, the delta, and the error
+func readBulkString(data []byte) (string, int, error) {
+	// first character $
+	pos := 1
+
+	// reading the length and forwarding the pos by
+	// the lenth of the integer + the first special character
+	len, delta := readLength(data[pos:])
+	pos += delta
+
+	// reading `len` bytes as string
+	return string(data[pos:(pos + len)]), pos + len + 2, nil
+}
+
+
+// reads a RESP encoded array from data and returns
+// the array, the delta, and the error
+func readArray(data []byte) (interface{}, int, error) {
+	// first character *
+	pos := 1
+
+	// reading the length
+	count, delta := readLength(data[pos:])
+	pos += delta
+
+	var elems []interface{} = make([]interface{}, count)
+	for i := range elems {
+		elem, delta, err := DecodeOne(data[pos:])
+		if err != nil {
+			return nil, 0, err
+		}
+		elems[i] = elem
+		pos += delta
+	}
+	return elems, pos, nil
+}
+
+
+func DecodeOne(data []byte) (interface{}, int, error) {
+	if len(data) == 0 {
+		return nil, 0, errors.New("no data")
+	}
+	switch data[0] {
+	case '+':
+		return readSimpleString(data)
+	case '-':
+		return readError(data)
+	case ':':
+		return readInt64(data)
+	case '$':
+		return readBulkString(data)
+	case '*':
+		return readArray(data)
+	}
+	return nil, 0, nil
+}
+
+func Decode(data []byte) (interface{}, error) {
+	if len(data) == 0 {
+		return nil, errors.New("no data")
+	}
+	value, _, err := DecodeOne(data)
+	return value, err
+}
